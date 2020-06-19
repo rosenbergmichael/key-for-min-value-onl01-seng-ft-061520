ee2319a5e43555b96fc132d1a5b362032277d10e
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-19 19:56:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,3 +6,6 @@ def key_for_min_value(name_hash)
 #  name_hash.each do |key|
     
 end
+
+
+#2. Build a method `key_for_min_value` that accepts an argument of a hash. This method should iterate over the hash and return the *key* (not the value!) that points to the smallest value of the set. If the method is called and passed an argument of an empty hash, it should return `nil`. 

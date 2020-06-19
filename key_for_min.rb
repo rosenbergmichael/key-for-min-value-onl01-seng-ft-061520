02f664574fe59b46dca63b484edfe236fe18613c
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-19 20:12:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
- 
+  name_hash.each do |name, number|
 binding.pry     
-end
+ end 
+ end
 
 
 #2. Build a method `key_for_min_value` that accepts an argument of a hash. This method should iterate over the hash and return the *key* (not the value!) that points to the smallest value of the set. If the method is called and passed an argument of an empty hash, it should return `nil`. 
